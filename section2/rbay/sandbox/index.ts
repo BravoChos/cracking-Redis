2f99271557e6d14be6b8e78b2903c520bb674d34
bravochos <bravochos@gmail.com>
2022-12-12 00:18:59 +0900

    uncorrect ways of HSET

--- section2/rbay/sandbox/index.ts
+++ section2/rbay/sandbox/index.ts
@@ -4,7 +4,17 @@ import { client } from '../src/services/redis';
 const run = async () => {
 	await client.hSet('car', {
 		color: 'red',
-		year: 1950
+		year: 1950,
+
+		// below will not cause an error...but doesn't store value the way we want
+		// { ... }.toString => "[Object objct]"
+		engine: {
+			cylinders: 8
+		},
+
+		// below will cause an error!!
+		owner: null, // null.toString() => X
+		service: undefined // undefined => X
 	});
 
 	const car = await client.hGetAll('car');
