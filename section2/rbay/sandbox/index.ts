8553707f4cf2d394ef8c49313e16ee71efc792c4
bravochos <bravochos@gmail.com>
2022-12-28 06:51:22 +0900

    running multiple commands at the same time

--- section2/rbay/sandbox/index.ts
+++ section2/rbay/sandbox/index.ts
@@ -2,20 +2,42 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	console.log('sldkfjsldkjf');
+	// await client.hSet('car', {
+	// 	color: 'red',
+	// 	year: 1950
+	// });
+
+	// const car = await client.hGetAll('car#123123123123');
+
+	// if (Object.keys(car.length === 0)) {
+	// 	console.log('Car not found, respond with 404');
+	// 	return;
+	// }
+
+	// // # note!
+	// // HGET always return emtpty object instead of null if the key doesn't exsist
+	// console.log(car);
+
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
 
-	const car = await client.hGetAll('car#123123123123');
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
 
-	if (Object.keys(car.length === 0)) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
+	const results = await Promise.all(commands);
 
-	// # note!
-	// HGET always return emtpty object instead of null if the key doesn't exsist
-	console.log(car);
+	console.log({ results });
 };
 run();
